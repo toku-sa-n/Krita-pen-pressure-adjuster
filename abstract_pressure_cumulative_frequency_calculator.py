8f2931fdff963bd5a56f9958d085368c8f243b5f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-17 21:37:36 +0900

    Normalized

--- abstract_pressure_cumulative_frequency_calculator.py
+++ abstract_pressure_cumulative_frequency_calculator.py
@@ -7,5 +7,5 @@ class AbstractPressureCumulativeFrequencyCalculator(ABC):
     @abstractmethod
     def calculate_pressure_cumulative_frequency(
         self, pen_pressures: list[NormalizedPressure]
-    ) -> list[tuple[float, NormalizedFrequency]]:
+    ) -> list[tuple[NormalizedPressure, NormalizedFrequency]]:
         pass
--- pressure_cumulative_frequency_calculator.py
+++ pressure_cumulative_frequency_calculator.py
@@ -11,13 +11,14 @@ class PressureCumulativeFrequencyCalculator(
 ):
     def calculate_pressure_cumulative_frequency(
         self, pen_pressures: list[NormalizedPressure]
-    ) -> list[tuple[float, NormalizedFrequency]]:
+    ) -> list[tuple[NormalizedPressure, NormalizedFrequency]]:
         pressures = list(map(lambda x: x.value, pen_pressures))
 
         scaled_pressures, frequencies = np.unique(pressures, return_counts=True)
         cumulative_frequencies = np.cumsum(frequencies)
         scaled_frequencies = cumulative_frequencies / max(cumulative_frequencies)
 
+        normalized_pressures = list(map(NormalizedPressure, scaled_pressures))
         normalized_frequencies = list(map(NormalizedFrequency, scaled_frequencies))
 
-        return list(zip(scaled_pressures, normalized_frequencies))
+        return list(zip(normalized_pressures, normalized_frequencies))
