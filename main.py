9f79ce883ee725cc77fce96047a8bd9c28779b09
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-17 23:36:11 +0900

    Split

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from scipy.interpolate import make_interp_spline
 from normalized_pressure import NormalizedPressure
 from typing import Any, Callable
 from abstract_normalized_pressure_input import AbstractNormalizedPressureInput
+from abstract_pressure_cumulative_frequency_calculator import (
+    AbstractPressureCumulativeFrequencyCalculator,
+)
+from abstract_krita_settings_writer import AbstractKritaSettingsWriter
 from evdev_pen_pressure_input import EvdevPenPressureInput
 from normalized_pressure_input import NormalizedPressureInput
 from krita_settings_writer_to_file import KritaSettingsWriterToFile
@@ -21,6 +25,7 @@ class AbstractGraphPlotter(ABC):
     @abstractmethod
     def plot_graph(
         self,
+        filename: str,
         frequencies: list[tuple[NormalizedPressure, NormalizedFrequency]],
     ) -> None:
         pass
@@ -32,12 +37,13 @@ class GraphPlotter(AbstractGraphPlotter):
 
     def plot_graph(
         self,
+        filename: str,
         frequencies: list[tuple[NormalizedPressure, NormalizedFrequency]],
     ) -> None:
         self.set_plot_properties()
         self.plot_original_data(frequencies)
         self.plot_bspline(frequencies)
-        self.save_graph()
+        self.save_graph(filename)
 
     def set_plot_properties(self) -> None:
         self.ax.set_title("Scaled Cumulative Pressure Frequency")
@@ -68,15 +74,9 @@ class GraphPlotter(AbstractGraphPlotter):
         self.ax.plot(x_bspline, y_bspline, color="red", label="B-Spline Curve")
         self.ax.legend()
 
-    def save_graph(self) -> None:
-        self.fig.savefig("graph.png")
+    def save_graph(self, filename: str) -> None:
+        self.fig.savefig(filename)
         print(f"\nGraph saved as graph.png")
-
-
-def reproduce_bspline_and_save(
-    coordinates: list[tuple[NormalizedPressure, NormalizedFrequency]], filename: str
-) -> None:
-    GraphPlotter().plot_graph(coordinates)
 
 
 def write_bspline_to_file(
@@ -86,29 +86,24 @@ def write_bspline_to_file(
     krita_settings_writer.write_settings(filename, coordinates)
 
 
-def create_pressure_graph(pen_pressures: list[NormalizedPressure]) -> None:
-    scaled_pressures_and_frequencies = (
-        PressureCumulativeFrequencyCalculator().calculate_pressure_cumulative_frequency(
-            pen_pressures
-        )
-    )
-
-    GraphPlotter().plot_graph(scaled_pressures_and_frequencies)
-
-    krita_settings_filename = "pen_pressure.txt"
-    krita_settings_writer = KritaSettingsWriterToFile()
-    krita_settings_writer.write_settings(
-        krita_settings_filename, scaled_pressures_and_frequencies
-    )
-
-
-def run(pressure_input: AbstractNormalizedPressureInput) -> None:
+def run(
+    pressure_input: AbstractNormalizedPressureInput,
+    freq_calculator: AbstractPressureCumulativeFrequencyCalculator,
+    graph_plotter: AbstractGraphPlotter,
+    config_writer: AbstractKritaSettingsWriter,
+) -> None:
     # Monitor pen pressure
     pen_pressures = pressure_input.monitor_pressure()
 
     # Create and display the pressure graph
     if pen_pressures:
-        create_pressure_graph(pen_pressures)
+        pressure_freq = freq_calculator.calculate_pressure_cumulative_frequency(
+            pen_pressures
+        )
+
+        graph_plotter.plot_graph("graph.png", pressure_freq)
+
+        config_writer.write_settings("pen_pressure.txt", pressure_freq)
 
 
 def main() -> None:
@@ -122,7 +117,12 @@ def main() -> None:
     pressure_input = EvdevPenPressureInput(input_path)
     normalized_pressure_input = NormalizedPressureInput(pressure_input)
 
-    run(normalized_pressure_input)
+    freq_calculator = PressureCumulativeFrequencyCalculator()
+
+    graph_plotter = GraphPlotter()
+    config_writer = KritaSettingsWriterToFile()
+
+    run(normalized_pressure_input, freq_calculator, graph_plotter, config_writer)
 
 
 if __name__ == "__main__":
