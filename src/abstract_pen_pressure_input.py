24273e217b1cd5c77712374ca9000c7038bdd2a9
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-18 09:32:06 +0900

    Move

--- src/abstract_pen_pressure_input.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from raw_pen_pressure import RawPenPressure
-
-
-class AbstractRawPenPressureInput(ABC):
-    @abstractmethod
-    def monitor_pressure(self) -> list[RawPenPressure] | None:
-        pass
